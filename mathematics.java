73e4583cd39ac670dd56ba0b0b3ec2e037893eae
MargaritaKostova <157585890+MargaritaKostova@users.noreply.github.com>
2024-01-24 12:11:16 +0200

    Hommework1

---
+++ mathematics.java
@@ -0,0 +1,11 @@
+package math;
+
+import java.util.Locale;
+
+public class mathematics {
+    public static void main(String[] args) {
+        String myName = "Margarita";
+        System.out.println(myName.length());
+        System.out.println(myName.toUpperCase());System.out.println(myName.toLowerCase());
+    }
+}
