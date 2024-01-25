a6475ee6b393efc15bddc334f4c9d174aeb1f8ea
MargaritaKostova <157585890+MargaritaKostova@users.noreply.github.com>
2024-01-25 10:26:53 +0200

    Add files via upload

--- mathematics.java
+++ mathematics.java
@@ -4,8 +4,11 @@ import java.util.Locale;
 
 public class mathematics {
     public static void main(String[] args) {
-        String myName = "Margarita";
-        System.out.println(myName.length());
-        System.out.println(myName.toUpperCase());System.out.println(myName.toLowerCase());
+
+        System.out.println(1 + 1);
+        System.out.println(Math.abs(-174));
+        System.out.println("174\t15\t0\t18");
+        System.out.println(Math.pow(-20,2));
+        System.out.println(Math.sqrt(353));
     }
 }
