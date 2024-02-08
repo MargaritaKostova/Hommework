0d85dcb62d39c2f34c64ea08a754d92779bd94df
MargaritaKostova <157585890+MargaritaKostova@users.noreply.github.com>
2024-02-08 17:31:51 +0200

    Add files via upload

--- mathematics.java
+++ mathematics.java
@@ -8,7 +8,7 @@ public class mathematics {
         System.out.println(myName);
         System.out.println(myName.toUpperCase());
         System.out.println(1 + 1);
-        System.out.println(Math.abs(-174));
+        System.out.println(Math.abs(-174)+" "+Math.abs(-15));
         System.out.println("174\t15\t0\t18");
         System.out.println(Math.pow(-20,2));
         System.out.println(Math.sqrt(353));
